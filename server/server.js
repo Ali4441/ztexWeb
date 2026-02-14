1b57917d5251d7b6f56a8fd5b641f7030e34a944
Ali4441 <amd444147@gmail.com>
2026-02-15 02:03:30 +0530

    fix env and server config

--- server/server.js
+++ server/server.js
@@ -6,30 +6,29 @@ const cors = require('cors');
 const spamRouter = require('./route/spamRoutes.js');
 
 const app = express();
+
 app.use(cors({
   origin: "http://localhost:5173",
   credentials: true,
   allowedHeaders: ["Content-Type", "Authorization"]
 }));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use('/api', router);
-
 app.use('/api/spam', spamRouter);
 
-
-
-
-
-//error handleer
+//error handler
 app.use(handleError);
+
 //connectDB
 connectDB().then(async () => {
   console.log("Database connected");
-  app.listen(8080, () => {
-    console.log("Server is running on port 8080");
-  });
+
+  const PORT = process.env.PORT || 5000;
+  app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
 }).catch(err => {
   console.log(" DB Connection Error:", err);
 });
