07386940b051205ddeafa95c8862cf5ab3548d33
Ali4441 <amd444147@gmail.com>
2026-02-15 21:48:49 +0530

    small change

--- server/utils/db.js
+++ server/utils/db.js
@@ -2,9 +2,13 @@ const mongoose = require('mongoose');
 
 module.exports = async () => {
   try {
-    const url = process.env.MONGO_URI || "mongodb://localhost:27017/ztexweb";
-
+    // const url = process.env.MONGO_URI || "mongodb://localhost:27017/ztexweb";
+    const url = "mongodb+srv://codewithali001_db_user:T2Ii7idfveGPnbaz@cluster1.lvymipp.mongodb.net/ztexweb?retryWrites=true&w=majority";
+    console.log("Connecting to MongoDB at:", url); // <-- for verification
     await mongoose.connect(url);
+
+
+
 
     console.log("Database connected successfully");
 
